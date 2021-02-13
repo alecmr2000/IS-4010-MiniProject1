c6cbf9579fa04b57251c5351248b04ac3a8611d1
alecmr2000 <alecmr2000@gmail.com>
2021-02-13 18:31:47 +0000

    Added opening lines.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+/* 
+Mini Project 1
+Group #_: YourName, YourPartnerName1, YourPartnerName2, YourPartnerName3 (if you have 4 members)
+Date: (whatever date you turn it in)
+*/
+
+import java.util.Random;
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+    public static void main(String[] args) {
+        System.out.println("Welcome to the guessing game! \nPlease enter any positive whole number: ");
+    }
+}  
